a8159ed3501c519f75dd98ef37d3f3161b5f08bc
Fujitsu <alchemikaslt1@gmail.com>
2023-12-18 19:51:35 +0200

    Add test funcions silutesmuziejusd_test() silutes_savdtest() lkca_test() pamarys_test2()

--- news_crawler/data_test.py
+++ news_crawler/data_test.py
@@ -1,5 +1,6 @@
 from lxml.etree import HTML
 from requests import get
+import re
 
 def silutesmuziejusd_test():
     data = get("https://www.silutesmuziejus.lt/home/apie-muzieju/naujienu-archyvas/").text
@@ -17,4 +18,80 @@ def silutesmuziejusd_test():
     print("Aprašymai:", "Gauta" if aprasymai else "Nepavyko gauti")
     print("Paveikslėliai:", "Gauta" if paveikslėliai else "Nepavyko gauti")
     print("Data:", "Gauta" if datos else "Nepavyko gauti")
+
+def silutes_savdtest():
+    data = get("https://www.silute.lt/naujienos/1091").text
+    data = HTML(data)
+
+    # Išgauti duomenis
+    pavadinimai = data.xpath("//span[@class='news_title']/text()")
+    aprasymai = data.xpath("//span[@class='news_description']/text()")
+    datas = data.xpath("//span[@class='date']//text()")
+    paveikslėliai = data.xpath("//span[@class='image_container']/img/@src")
+
+    # Patikrinti ir spausdinti ataskaitą
+    print("Duomenys is silute.lt:")
+    print("Pavadinimai:", "Gauta" if pavadinimai else "Nepavyko gauti")
+    print("Aprašymai:", "Gauta" if aprasymai else "Nepavyko gauti")
+    print("Paveikslėliai:", "Gauta" if paveikslėliai else "Nepavyko gauti")
+    print("Data:", "Gauta" if datas else "Nepavyko gauti")
+
+
+def lkca_test():
+    data = get("https://lkca.lt/naujienos/").text
+    data = HTML(data)
+
+    # Išgauti duomenis
+    pavadinimai = data.xpath("//div[@class='post-header']/h3/a/text()")
+    aprasymai = data.xpath("//div[@class='excerpt']/text()")
+    datas = data.xpath("//div[@class='grav-wrap']/div[@class='text']/span/text()")
+    paveikslėliai = data.xpath("//span[@class='post-featured-img']/img/@src")
+
+    # Patikrinti ir spausdinti ataskaitą
+    print("Duomenys iš lkca.lt:")
+    print("Pavadinimai:", "Gauta" if pavadinimai else "Nepavyko gauti")
+    print("Aprašymai:", "Gauta" if aprasymai else "Nepavyko gauti")
+    print("Paveikslėliai:", "Gauta" if paveikslėliai else "Nepavyko gauti")
+    print("Data:", "Gauta" if datas else "Nepavyko gauti")
+
+def pamarys_test():
+    url = "http://www.pamarys.eu/category/aktualijos/"
+
+    # Nustatome pasirinktines antraštes
+    custom_headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+        'Accept-Language': 'en-US,en;q=0.5',
+        # Čia galite pridėti kitas antraštes, jei reikia
+    }
+
+    response = get(url, headers=custom_headers)
+
+    if response.status_code == 200:
+        data = HTML(response.text)
+
+        # Išgauti duomenis
+        pavadinimai = data.xpath("//h2[@class='post-title']/a/text()")
+        aprasymai = data.xpath("//div[@class='postmeta']/span[@class='meta-author sep']/a/following-sibling::text()")
+        datas = data.xpath("//span[@class='meta-date']/a/time/@datetime")
+        paveikslėliai = data.xpath("//div[contains(@class, 'post-thumbnail')]/a/img/@src")
+
+        # Patikrinti ir spausdinti ataskaitą
+        print("Duomenys iš pamarys.eu:")
+        print("Pavadinimai:", "Gauta" if pavadinimai else "Nepavyko gauti")
+        print("Aprašymai:", "Gauta" if aprasymai else "Nepavyko gauti")
+        print("Data:", "Gauta" if datas else "Nepavyko gauti")
+        print("Paveikslėliai:", "Gauta" if paveikslėliai else "Nepavyko gauti")
+    else:
+        print("Nepavyko prisijungti prie puslapio. Status kodas:", response.status_code)
+
+
+
+
+
+
+
+
+
+
+
 
--- news_crawler/main.py
+++ news_crawler/main.py
@@ -1,6 +1,11 @@
-
 from request_test import test_urls
 from data_test import silutesmuziejusd_test
+from data_test import silutes_savdtest
+from data_test import lkca_test
+from data_test import pamarys_test2
 
 silutesmuziejusd_test()
-test_urls()
+silutes_savdtest()
+lkca_test()
+pamarys_test2()
+#test_urls()
