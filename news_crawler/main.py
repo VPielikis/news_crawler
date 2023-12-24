4c3e3c3dedb7147643925198b70f4d69eb5862cb
Vilmantas <alchemikaslt1@gmail.com>
2023-12-24 17:19:58 +0200

    Created funcion def logika() to main.py

--- news_crawler/main.py
+++ news_crawler/main.py
@@ -1,7 +1,21 @@
 from category_crawler import *
 
-kategorijos = kategoriju_crawler()
-print("Gautos kategorijos:")
-for kategorija in kategorijos:
-    print(kategorija)
-irasyti_i_csv(kategorijos)
+def logika(msg):
+    while True:
+        atsakymas = input(msg).upper()
+        if atsakymas in ["Y", "N"]:
+            return atsakymas
+        print("Prašome įvesti 'Y' arba 'N'.")
+
+atsakymas = logika("Ar norite pradėti kategorijų gavimą iš tinklapio? (Y/N): ")
+if atsakymas == "Y":
+    kategorijos = kategoriju_crawler()
+
+
+    print("Gautos kategorijos:")
+    for kategorija in kategorijos:
+        print(kategorija)
+
+    atsakymas = logika("Ar norite įrašyti gautas kategorijas į CSV failą? (Y/N): ")
+    if atsakymas == "Y":
+        irasyti_i_csv(kategorijos)
